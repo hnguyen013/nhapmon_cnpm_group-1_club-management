d4971b6790a7347a27fedc1c1b448c12b6b305e0
Nguyễn Võ Yên Khanh <kn22042006@gmail.com>
2025-12-09 10:39:53 +0700

    Implement US-H2.1: add public layout (base_home + club pages)

--- portal/views/__init__.py
+++ portal/views/__init__.py
@@ -1,5 +1,14 @@
-# portal/views/__init__.py
-
+# Import views authentication
 from .auth import login_view, logout_view
 
-__all__ = ["login_view", "logout_view"]
+# Import views cho các trang public
+from .public import home, club_list, club_detail
+
+# Export các view (cho Django dùng)
+__all__ = [
+    "login_view",
+    "logout_view",
+    "home",
+    "club_list",
+    "club_detail",
+]
