d011a9ba345a69238437ecfdfd913b7c2c89b617
Your Name <tranlebaonghi2105@gmail.com>
2026-01-02 19:01:05 +0700

    feat(events): add public URL /events/ for events list

--- portal/urls/public.py
+++ portal/urls/public.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", public_views.home, name="home"),
     path("clubs/", public_views.club_list, name="club_list"),
     path("clubs/<int:club_id>/", public_views.club_detail, name="club_detail"),
+
+    # US-C1.1 - Public events list
+    path("events/", public_views.event_list, name="event_list"),
 ]
